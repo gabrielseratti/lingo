28335941d253abe18288b4c4a857e8cad0bd539b
Gabriel Seratti <gserattioliveira@gmail.com>
2024-05-22 21:05:18 -0300

    6:17

--- app/lesson/question-buble.tsx
+++ app/lesson/question-buble.tsx
@@ -4,7 +4,7 @@ type Props = {
     question:string;
 };
 
-export const QuestionBuble = ({}: Props) => {
+export const QuestionBuble = ({ question }: Props) => {
     return (
         <div className="flex items-center gap-x-4 mb-6">
             <Image 
@@ -14,6 +14,17 @@ export const QuestionBuble = ({}: Props) => {
                 width={60}
                 className="hidden lg:block"
             />
+            <Image 
+                src={"/mascot.svg"}
+                alt="Mascot"
+                height={40}
+                width={40}
+                className="block lg:hidden"
+            />
+            <div className="relative py-2 px-4 border-2 ronded-xls text-sm lg:text-base">
+                {question}
+                <div className="absolute -left-3 top-1/2 w-0 h-0 border-x-8 border-x-transparent border-t-8 transform -translate-y-1/2 rotate-90" />
+            </div>
         </div>
     )
 }
